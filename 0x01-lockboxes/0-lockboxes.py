db85ca8ad410535826dad05477006f7f8b1ad8fa
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2023-02-02 19:45:21 +0100

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,24 +4,21 @@ Locked Boxes
 """
 
 def canUnlockAll(boxes):
-     '''
-    Determines if all the boxes can be opened
     '''
-    # Set to keep track of the boxes that can be opened
-    opened = set([0])
-    # List to store the keys of the current box
-    keys = boxes[0]
-    # Iterate until all boxes have been opened
-    while len(opened) < len(boxes):
-        # Iterate through the keys of the current box
-        for key in keys:
-            if 0 <= key < len(boxes) and key not in opened:
-                # Add the key to the set of opened boxes
-                opened.add(key)
-                # Add the keys of the newly opened box to the list of keys
-                keys += boxes[key]
-        # If no new boxes were opened, all boxes cannot be opened
-        if len(opened) == len(boxes):
-            return True
-    return False
+    determines if all the boxes can be opened.
+    '''
+    if len(boxes[0]) == 0:
+        return (False)
+    keys = {0}
+    size = len(boxes)
+    visited = {0}
+    keys = keys.union(boxes[0])
+    while size > 0:
+        for i in keys:
+            if i in visited:
+                continue
+            keys = keys.union(boxes[i])
+            visited.add(i)
+        size -= 1
+    return len(keys) == len(boxes)
 
